ca187a57b3860f344d38fe69088ee57fd5bf1f72
Augusto Grassi <augustograssi0101@gmail.com>
2024-05-03 14:30:08 -0300

    commit

--- data/schema.sql
+++ data/schema.sql
@@ -1,11 +1,6 @@
-CREATE TABLE task (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(100) NOT NULL,
-    descriptionn VARCHAR(100) NOT NULL,
-    statuss VARCHAR(100) NOT NULL,
-    creation_date DATE DEFAULT (DATE('now')) NOT NULL
-);
+CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(100) NOT NULL, descriptionn varchar(100) NOT NULL, creation_date varchar(100) NOT NULL, statuss varchar(100) NOT NULL);
 
-INSERT INTO task (title,descriptionn,statuss) VALUES ('Office Meeting', 'About profit','pendente');
-INSERT INTO task (title,descriptionn,statuss) VALUES ('Office Meeting', 'About investment','in progress');
-INSERT INTO task (title,descriptionn,statuss) VALUES ('Online Meeting', 'About new partners','completed');
+
+INSERT INTO task (title,descriptionn,creation_date,statuss) VALUES ('Office Meeting', 'About profit','20-04-2024','pendente');
+INSERT INTO task (title,descriptionn,creation_date,statuss) VALUES ('Office Meeting', 'About investment','30-04-2024','in progress');
+INSERT INTO task (title,descriptionn,creation_date,statuss) VALUES ('Online Meeting', 'About new partners','01-05-2024','completed');
